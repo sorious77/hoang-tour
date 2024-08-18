f24e243f9f92605bd8561690ff8449588af3fa31
sorious77 <sorious77@naver.com>
2024-08-18 17:04:38 +0900

    :closed_lock_with_key: Naver Map API 키 환경변수 등록

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,12 @@ import {NavermapsProvider} from 'react-naver-maps';
 
 
 export default function App({Component, pageProps}: AppProps) {
+    const ncpClientId: string = process.env.NEXT_PUBLIC_NAVER_MAP_CLIENT_ID as string | ""
+
+    console.log(ncpClientId);
+
     return (
-        <NavermapsProvider ncpClientId="p967kw7fg2">
+        <NavermapsProvider ncpClientId={ncpClientId}>
             <Layout>
                 <Component {...pageProps} />
             </Layout>
